bb840bb85941d4f1827813ab5137ceb63d08a8a3
Jenny Zonka <jennycade@gmail.com>
2021-06-24 14:48:07 -0700

    Remove onClick from checkboxes in Someday

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 import DailyListPane from './DailyListPane';
 import BucketPane from './BucketPane';
 
 const App = () => {
-  const [message, setMessage] = useState('');
-  const addToToday = (title) => {
-
-  }
   return (
     <div className = 'app'>
       <DailyListPane />
--- src/CompletedBox.js
+++ src/CompletedBox.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import DoneIcon from '@material-ui/icons/Done';
-import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
-import CloseIcon from '@material-ui/icons/Close';
 
 const CompletedBox = (props) => {
   return (
     <span
       className = 'completed_box'
-      onClick={ () => { props.toggleTodoCompleted(props.id) } }
+      onClick={ props.toggleTodoCompleted ? () => { props.toggleTodoCompleted(props.id) } : () => null }
     >
       { props.completed ? <DoneIcon /> : <CheckBoxOutlineBlankIcon /> }
     </span>
